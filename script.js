7395c928a8b7fcbe4d57a349f3b08ecb7476a18c
CKRefrigeracion <bboywinner88@gmail.com>
2026-02-04 13:23:34 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -21,3 +21,16 @@ window.addEventListener("load", showOnScroll);
 
 // Ejecutar al hacer scroll
 window.addEventListener("scroll", showOnScroll);
+
+const toggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+toggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
+navLinks.querySelectorAll("a").forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("show");
+  });
+});
